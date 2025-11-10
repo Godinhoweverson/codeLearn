18ac6be924fdde0f0f3af73b7b264f69e0364405
Godinhoweverson <weverson.godinho@icloud.com>
2025-11-10 16:01:08 +0000

    iniated the sign in page and added style

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
+
+// KEEP THE COPYRIGHT DATE UPDATED
 document.getElementById("year").textContent = new Date().getFullYear();
+
+
+// TO PREVENT THE PAGE REFRESH WHEN CLICK IN JOIN BUTTON ON FOOTER
+document.getElementsByClassName("footer-btn")[0].addEventListener("click", function(event){
+  event.preventDefault()
+});
+
+// SIGNIN FORM
+let signIn = document.getElementById("signIn-form");
+console.log(signIn)
+signIn.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    if(e.target.fname.value == ""){
+        alert("yes")
+    }
+})
