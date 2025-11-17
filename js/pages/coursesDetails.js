b8c1bacd158e26596948bd71201a7768136d23de
Weverson Godinho <weverson.godinho@icloud.com>
2025-11-17 21:58:32 +0000

    added the style on courseDetail page

--- js/pages/coursesDetails.js
+++ js/pages/coursesDetails.js
@@ -1,11 +1,13 @@
 let courseId = localStorage.getItem("courseId");
-let description = document.getElementsByClassName("courseDetail-description-content")[0];
+let description = document.getElementById(
+  "courseDetail-description"
+);
 
 fetch("../database/courseDataSet.json")
 .then((res)=> res.json())
 .then((courses)=>{
    const course = courses.filter((course) => course.course_id === Number(courseId));
 
-   description.innerHTML = `${courses[0].description}`
+   description.innerHTML = `${courses[4].description}`
 
 })
