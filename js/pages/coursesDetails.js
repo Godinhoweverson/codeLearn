c06777176dde1d3b965e51e09b48866d5f15724f
Godinhoweverson <weverson.godinho@icloud.com>
2025-11-18 15:38:33 +0000

    change the html and css from course details page

--- js/pages/coursesDetails.js
+++ js/pages/coursesDetails.js
@@ -1,4 +1,11 @@
 let courseId = localStorage.getItem("courseId");
+let heading = document.getElementById("heading");
+let img = document.getElementsByClassName("courseDetail-main-course-card-img")[0];
+let lectures = document.getElementById("lectures");
+let Subcribes = document.getElementById("Subcribes");
+let reviews = document.getElementById("course-reviews");
+
+
 let description = document.getElementById(
   "courseDetail-description"
 );
@@ -6,8 +13,14 @@ let description = document.getElementById(
 fetch("../database/courseDataSet.json")
 .then((res)=> res.json())
 .then((courses)=>{
-   const course = courses.filter((course) => course.course_id === Number(courseId));
+  const course = courses.filter((course) => course.course_id === Number(courseId));
+  
+  heading.textContent = `${course[0].course_title}`
+  img.src=`../assets/images/courses/${course[0].image}`;
+  img.alt = `${course[0].main_technology} programming icon`;
+  lectures.textContent = `${course[0].num_lectures}`;
+  Subcribes.textContent = `${course[0].num_lectures}`;
+  reviews.textContent = `${course[0].num_lectures}`;
 
-   description.innerHTML = `${courses[4].description}`
-
+  description.innerHTML = `${course[0].description}`
 })
